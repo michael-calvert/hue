b6bb542aff23f0afdf27460437767ecbe2491d87
Roma Slyusarchuk <rslyusarchuk@maprtech.com>
2014-12-16 14:25:22 +0200

    [FIX] autoinstall protobuf dependency (cherry picked from commit 6d9ae6d)

--- bin/hue.sh
+++ bin/hue.sh
@@ -8,6 +8,7 @@ export HUE_HOME=${bin}/..
 # activate the Python virtual environment
 #
 source $HUE_HOME/build/env/bin/activate
+pip install protobuf
 export LD_LIBRARY_PATH=$HUE_HOME/build/env/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/mapr/lib:$JAVA_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH
 
