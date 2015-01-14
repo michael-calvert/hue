0daaa81bc26368e3a0459da33ce96c48aa56cb0d
Oleksiy Sayankin <osayankin@maprtech.com>
2015-01-14 17:59:12 +0200

    [Fixed] Moved keys generation to start case.

--- bin/hue.sh
+++ bin/hue.sh
@@ -40,8 +40,6 @@ log=$HUE_LOG_DIR/hue-$HUE_IDENT_STRING-$command-$HOSTNAME.out
 pid=$HUE_PID_DIR/hue-$HUE_IDENT_STRING-$command.pid
 secure_sh_log=$HUE_LOG_DIR/secure-sh-log.out
 
-# Generate secure keys
-nohup $HUE_HOME/bin/secure.sh >> "$secure_sh_log" 2>&1 < /dev/null &
 
 case $startStop in
   (start)
@@ -52,6 +50,8 @@ case $startStop in
     fi
   fi
   cd $HUE_HOME
+  # Generate secure keys
+  nohup $HUE_HOME/bin/secure.sh >> "$secure_sh_log" 2>&1 < /dev/null &
   nohup $HUE_HOME/build/env/bin/hue $command >> "$log" 2>&1 < /dev/null &
   echo $! > $pid
   echo "`date` $command started, pid `cat $pid`" >> "$log" 2>&1 < /dev/null
