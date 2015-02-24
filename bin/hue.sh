477b662cf93a34c5372ae36ee1d85f199b50294a
ntarasenko <ntarasenko@maprtech.com>
2015-02-24 23:38:53 +0200

    [FIX] add export JAVA_HOME for maprsecurity.so

--- bin/hue.sh
+++ bin/hue.sh
@@ -9,6 +9,7 @@ export HUE_HOME=${bin}/..
 #
 source $HUE_HOME/build/env/bin/activate
 pip install protobuf
+export JAVA_HOME=`readlink -f /usr/bin/java | sed "s:jre/bin/java::"`
 export LD_LIBRARY_PATH=$HUE_HOME/build/env/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/opt/mapr/lib:$JAVA_HOME/jre/lib/amd64/server:$LD_LIBRARY_PATH
 
